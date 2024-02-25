3d5d1af873f35e82398fc0eb412672591f8e3f2e
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-02-25 21:39:19 +0000

    Add Home page logo and attemped responsive layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "Our House",
+  title: "Our House App",
   description: "The one stop app",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
