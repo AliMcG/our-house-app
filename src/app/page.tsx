e46d83bb91a40ce316935fac41aef74bc5c3021f
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-02-29 11:45:12 +0000

    Update responsive breakpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-white text-slate-800 md:items-center md:justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 md:py-16">
-        <div className="flex flex-col-reverse md:flex-row md:flex-wrap ">
+        <div className="flex flex-col-reverse xl:flex-row md:flex-wrap ">
           <div className="mt-4 flex flex-col justify-center">
-            <h1 className="flex items-center justify-center text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            <h1 className="flex items-center justify-center text-5xl font-extrabold tracking-tight md:text-[5rem]">
               Our House
             </h1>
             <div className="mt-4 flex flex-col items-center justify-center text-lg">
