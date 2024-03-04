f33a70e508207827fd1d5cbc31030b11e8732d7f
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-03-04 20:54:10 +0000

    Update to use size and type

--- src/app/_components/AuthenticationButton.tsx
+++ src/app/_components/AuthenticationButton.tsx
@@ -3,14 +3,16 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Button from "@/app/_components/Button";
 
-export default function AuthShowcase() {
+export type Size = "small" | "normal" | "large"
+
+export default function AuthShowcase({ size }: { size: Size}) {
   const { data: sessionData } = useSession();
 
   return (
     <Button
       text={sessionData ? "Sign out" : "Sign in"}
       intent={"primary"}
-      size={"large"}
+      size={`${size}`}
       onClick={sessionData ? () => void signOut() : () => void signIn("google")}
     />
   );
