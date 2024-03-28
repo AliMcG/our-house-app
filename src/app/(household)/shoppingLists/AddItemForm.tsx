5c3b265074347ec10a0e615bc7994edaa3fef63d
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-03-28 21:12:00 +0000

    Update AddItemForm to use updated GenericForm and form components

--- src/app/(household)/shoppingLists/AddItemForm.tsx
+++ src/app/(household)/shoppingLists/AddItemForm.tsx
@@ -1,21 +1,39 @@
 "use client";
 
 import * as z from "zod";
-import GenericForm from "@/app/_components/GenericForm";
+import GenericForm from "@/app/_components/form/GenericForm";
+import Inputfield from "@/app/_components/form/InputFiled";
+import Button from "@/app/_components/Button";
+import FormItem from "@/app/_components/form/FormItem";
+import FormLabel from "@/app/_components/form/FormLabel";
 
-
-const schema = z.object({
-  item: z.string().min(3, { message: 'Too short' }).max(96, { message: 'Too long' })
-});
-
-export default function AddItemForm():JSX.Element {
-  function handleSubmit(data: z.infer<typeof schema>) {
+export default function AddItemForm() {
+  const formSchema = z.object({
+    name: z.string().min(1, { message: "Name is required" }),
+  });
+  function handleSubmit(data: z.infer<typeof formSchema>) {
     console.log("SUBMITTING ITEM TO ADD: ", data);
   }
 
   return (
-    <div>
-      <GenericForm schema={schema} handleSubmit={handleSubmit} />
-    </div>
-  )
+    <GenericForm
+      formSchema={formSchema}
+      handleSubmit={handleSubmit}
+      className="flex flex-col w-80 justify-center items-center border-gray-400 border rounded-md p-4"
+    >
+      <FormItem>
+        <FormLabel fieldName={"name"}>Shopping list name</FormLabel>
+        <Inputfield
+          fieldName={"name"}
+          placeholder="Enter a name..."
+          className="w-40"
+        />
+      </FormItem>
+      <FormItem className="flex justify-end">
+        <Button type="submit" className="w-20">
+          Submit
+        </Button>
+      </FormItem>
+    </GenericForm>
+  );
 }
