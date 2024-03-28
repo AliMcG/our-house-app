e9ae4eb4e96ad046565cb8b8f5b980970ce91dcc
webshuriken <webshuriken@gmail.com>
2024-03-28 15:42:50 +0000

    implement form resolver with Zod TS schema declaration

--- src/app/_components/AddItemForm.tsx
+++ src/app/_components/AddItemForm.tsx
@@ -1,10 +1,19 @@
 "use client";
 
-import { useForm, FormProvider, useFormContext } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
+
+const schema = z.object({
+  item: z.string().min(3, { message: 'Too short' }).max(96, { message: 'Too long' })
+});
 
 export default function AddItemForm(): JSX.Element {
-  const methods = useForm()
+  const methods = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema)
+  });
+  const { errors } = methods.formState;
 
   // just a place holder until it is passed in as a prop
   function onSubmit(data) {
@@ -15,7 +24,8 @@ export default function AddItemForm(): JSX.Element {
     <div>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <FormInput />
+          <input type="text" placeholder="enter item to add" {...methods.register("item")} />
+          {errors.item?.message && <p>{errors.item?.message}</p>}
           <input type="submit" />
         </form>
       </FormProvider>
@@ -23,7 +33,3 @@ export default function AddItemForm(): JSX.Element {
   );
 }
 
-function FormInput() {
-  const { register } = useFormContext();
-  return <input type="text" placeholder="enter item to add" {...register("item")} />;
-}
