faf5849b209c7c95212873991421b407343630cd
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-03-28 21:10:49 +0000

    Add InputField component

---
+++ src/app/_components/form/InputFiled.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React from "react";
+import { useFormContext } from "react-hook-form";
+import { cn } from "@/app/utils/cn";
+
+export interface InputFieldProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  fieldName: string;
+}
+
+export default function Inputfield({
+  fieldName,
+  type,
+  className,
+  ...rest
+}: InputFieldProps) {
+  const { register, getFieldState } = useFormContext();
+  const filedState = getFieldState(fieldName);
+
+  return (
+    <>
+      <input
+        {...register(fieldName)}
+        type={type}
+        className={cn(
+          "border-input flex h-10 w-full rounded-md border px-3 py-3 text-sm file:border-0 file:bg-transparent file:font-medium placeholder:text-slate-400",
+          className,
+        )}
+        {...rest}
+      />
+      <p className="text-red-700">{filedState.error?.message?.toString()}</p>
+    </>
+  );
+}
