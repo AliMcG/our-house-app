d03ce6427481af679a5037c0632e2a87acd02350
webshuriken <webshuriken@gmail.com>
2024-03-28 11:39:33 +0000

    style the Home component header to be responsive

--- src/app/(household)/home/page.tsx
+++ src/app/(household)/home/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <article className="flex min-h-screen flex-col pt-16 bg-white text-slate-800">
       <header className="flex items-center justify-center p-4">
-        <h1 className="text-center text-5xl font-extrabold tracking-tight md:text-[5rem]">
+      <h1 className="text-center text-3xl font-extrabold tracking-tight md:text-[2rem] lg:text-[3rem] xl:text-[4rem]">
           HOUSEHOLD
         </h1>
       </header>
