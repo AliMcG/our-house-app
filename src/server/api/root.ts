ec2ee9d4e41be42ed741fe3f11d2e01402848511
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-03-29 12:07:08 +0000

    Add shoppingListRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { postRouter } from "@/server/api/routers/post";
+import { shoppingListRouter } from "@/server/api/routers/shoppingList";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  shoppingList: shoppingListRouter
 });
 
 // export type definition of API
