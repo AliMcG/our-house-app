8f44cd084bea8dba2713bf626bf31bb483064c14
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-03-29 15:26:49 +0000

    Cypress Google sign works with useSession - sets the session user from Google api call.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -33,43 +33,47 @@
 declare global {
   namespace Cypress {
     interface Chainable {
-      loginByGoogleApi(): Chainable<void>
+      loginByGoogleApi(): Chainable<void>;
     }
   }
 }
 
 // cypress/support/commands.js
-Cypress.Commands.add('loginByGoogleApi', () => {
-  cy.log('Logging in to Google')
+Cypress.Commands.add("loginByGoogleApi", () => {
+  cy.log("Logging in to Google");
   /** Programmatically logs into Google with NextAuth
    * https://docs.cypress.io/guides/end-to-end-testing/google-authentication#Custom-Command-for-Google-Authentication
    */
   cy.request({
-    method: 'POST',
-    url: 'https://www.googleapis.com/oauth2/v4/token',
+    method: "POST",
+    url: "https://www.googleapis.com/oauth2/v4/token",
     body: {
-      grant_type: 'refresh_token',
-      client_id: Cypress.env('googleClientId'),
-      client_secret: Cypress.env('googleClientSecret'),
-      refresh_token: Cypress.env('googleRefreshToken'),
+      grant_type: "refresh_token",
+      client_id: Cypress.env("googleClientId"),
+      client_secret: Cypress.env("googleClientSecret"),
+      refresh_token: Cypress.env("googleRefreshToken"),
     },
   }).then(({ body }) => {
-    const { access_token, id_token } = body
-    console.log(body.user)
+    const { access_token, id_token } = body;
 
     cy.request({
-      method: 'GET',
-      url: 'https://www.googleapis.com/oauth2/v3/userinfo',
+      method: "GET",
+      url: "https://www.googleapis.com/oauth2/v3/userinfo",
       headers: { Authorization: `Bearer ${access_token}` },
     }).then(({ body }) => {
-
-      /** sets the session cookie and then intercepts the session to add the user role 
+      const user = body;
+      /** sets the session cookie and then intercepts the auth api call to return the session user.
        * https://www.youtube.com/watch?v=SzhulGxprCw
-      */
-      cy.setCookie('next-auth.session-token', id_token)
-      cy.intercept('api/auth/session', {status: 200}).as('next-auth')
-
-    })
-
-  })
-})
+       */
+      cy.setCookie("next-auth.session-token", id_token);
+      cy.intercept('api/auth/session', (req) => {
+        req.reply({
+          status: 200,
+          body: {
+            user
+          }
+        })
+      }).as('next-auth')
+    });
+  });
+});
