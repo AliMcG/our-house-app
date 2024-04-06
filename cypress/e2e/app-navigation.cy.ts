c09eb3492206fb0586771d07bb5ef4d6cfa32b21
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-04-06 21:21:50 +0100

    Add site navigation functional tests

--- cypress/e2e/app-navigation.cy.ts
+++ cypress/e2e/app-navigation.cy.ts
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Site navigation', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.loginByGoogleApi()
+    cy.wait(2000)
+  })
+
+  afterEach(() => {
+    cy.logout()
+  })
+
+  it('visit home page', () => {
     cy.visit('http://localhost:3000/home')
+  })
+  it('visit Shopping list page', () => {
+    cy.visit('http://localhost:3000/shoppingLists')
+  })
+  it('visit Chores page', () => {
+    cy.visit('http://localhost:3000/chores')
   })
 })
