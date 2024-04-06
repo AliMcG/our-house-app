164f3dfd1bf6d1117fb0fccbe34db926bd7f6e03
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-04-06 11:41:17 +0100

    Add extra ? to the optional chain

--- src/app/_components/TopNavigationBar.tsx
+++ src/app/_components/TopNavigationBar.tsx
@@ -9,12 +9,12 @@ import HomePageImage from "@/public/home_logo_final.svg";
 export default function TopNavigationBar(): JSX.Element {
   const session = useSession();
   // if the user has an image, use it, otherwise use the app logo
-  const imgSrc = session.data?.user.image ?? (HomePageImage as string);
+  const imgSrc = session.data?.user?.image ?? (HomePageImage as string);
 
   return (
     <aside className="flex justify-end h-8 w-screen mt-4 px-4 bg-fuchsia-500">
       <div className="flex items-center mr-4">
-        <h3>{session.data?.user.name}</h3>
+        <h3>{session.data?.user?.name}</h3>
       </div>
       <Link 
         href={"/"} 
