c7247cccf61a73f5371a77792b22742af05ec361
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-04-07 09:39:27 +0100

    Add assertion to navigation tests

--- cypress/e2e/app-navigation.cy.ts
+++ cypress/e2e/app-navigation.cy.ts
@@ -11,11 +11,14 @@ describe('Site navigation', () => {
 
   it('visit home page', () => {
     cy.visit('http://localhost:3000/home')
+    cy.url().should('include', '/home')
   })
   it('visit Shopping list page', () => {
     cy.visit('http://localhost:3000/shoppingLists')
+    cy.url().should('include', '/shoppingLists')
   })
   it('visit Chores page', () => {
     cy.visit('http://localhost:3000/chores')
+    cy.url().should('include', '/chores')
   })
 })
