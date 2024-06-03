a893c57ec2e5c496eec02ec8903c70c99a10e91a
webshuriken <webshuriken@gmail.com>
2024-06-03 17:21:51 +0100

    add function to component that updates a list item as active or not
    file: ItemsListCard.tsx

--- src/app/(household)/shoppingLists/[slug]/ItemsListCard.tsx
+++ src/app/(household)/shoppingLists/[slug]/ItemsListCard.tsx
@@ -5,7 +5,10 @@ import Card from "@/app/_components/Card";
 import Button from "@/app/_components/Button";
 import { useRouter } from "next/navigation";
 import { api } from "@/trpc/react";
-import { XCircleIcon } from "@heroicons/react/24/solid";
+import { 
+  XCircleIcon,
+  CheckCircleIcon,
+} from "@heroicons/react/24/solid";
 import type { ShoppingItem } from "@prisma/client";
 
 
@@ -22,15 +25,37 @@ export default function ItemsList({
     },
   });
 
+  // updateActive Mutation
+  const { mutate: updateActiveMutate } = api.shoppingListItem.updateActive.useMutation({
+    onSuccess: (response) => {
+      console.log("UPDATED ACTIVE ITEM STATUS", response);
+      router.refresh();
+    },
+  });
+
   // removes the item from the database
   const handleDeleteItem = (id: string) => {
     deleteMutate({ id: id });
   };
 
+  // update the item to active or not active
+  // the item will have a strike through if it is not active
+  const handleUpdateActive = (id: string) => {
+    console.log("MARK ITEM AS COMPLETE: ", id);
+    updateActiveMutate({ id: id, active: !shoppingItem.active });
+  }
+
   // TODO: Style component to look like an item in a list
   return (
     <Card>
       {shoppingItem.name}
+      <Button
+        type="button"
+        className="h-8 w-8"
+        onClick={() => handleUpdateActive(shoppingItem.id)}
+      >
+        <CheckCircleIcon />
+      </Button>
       <Button
         type="button"
         className="h-8 w-8"
