51f02896cebe1451a105e5bee31ff7080083bc9f
webshuriken <webshuriken@gmail.com>
2024-06-03 13:54:39 +0100

    add ItemsListCard component, to display each item in a list with delete button

---
+++ src/app/(household)/shoppingLists/[slug]/ItemsListCard.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import Card from "@/app/_components/Card";
+import Button from "@/app/_components/Button";
+import { useRouter } from "next/navigation";
+import { api } from "@/trpc/react";
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import type { ShoppingItem } from "@prisma/client";
+
+
+export default function ItemsList({
+  shoppingItem,
+}: {
+  shoppingItem: ShoppingItem;
+}) {
+  const router = useRouter();
+  const { mutate } = api.shoppingListItem.delete.useMutation({
+    onSuccess: (response) => {
+      console.log("DELETED list item", response);
+      router.refresh();
+    },
+  });
+
+  // removes the item from the database
+  const deleteItem = (id: string) => {
+    mutate({ id: id });
+  };
+
+  // TODO: Style component to look like an item in a list
+  return (
+    <Card>
+      {shoppingItem.name}
+      <Button
+        type="button"
+        className="h-8 w-8"
+        onClick={() => deleteItem(shoppingItem.id)}
+      >
+        <XCircleIcon />
+      </Button>
+    </Card>
+  );
+}
