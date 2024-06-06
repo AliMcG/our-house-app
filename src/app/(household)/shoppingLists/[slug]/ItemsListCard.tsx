bd3d2ae85163ea3351dec0b9d57338b0858583c3
webshuriken <webshuriken@gmail.com>
2024-06-06 19:18:37 +0100

    move all functions to parent element only pass in the required functionality

--- src/app/(household)/shoppingLists/[slug]/ItemsListCard.tsx
+++ src/app/(household)/shoppingLists/[slug]/ItemsListCard.tsx
@@ -3,8 +3,6 @@
 import React from "react";
 import Card from "@/app/_components/Card";
 import Button from "@/app/_components/Button";
-import { useRouter } from "next/navigation";
-import { api } from "@/trpc/react";
 import { 
   XCircleIcon,
   CheckCircleIcon,
@@ -12,39 +10,16 @@ import {
 import type { ShoppingItem } from "@prisma/client";
 
 
-export default function ItemsList({
+export default function ItemsListCard({
   shoppingItem,
+  deleteItem,
+  updateActive
 }: {
   shoppingItem: ShoppingItem;
+  deleteItem: (id: string) => void;
+  updateActive: (id: string, active: boolean) => void;
 }) {
-  const router = useRouter();
-  const { mutate: deleteMutate } = api.shoppingListItem.delete.useMutation({
-    onSuccess: (response) => {
-      console.log("DELETED list item", response);
-      router.refresh();
-    },
-  });
-
-  // updateActive Mutation
-  const { mutate: updateActiveMutate } = api.shoppingListItem.updateActive.useMutation({
-    onSuccess: (response) => {
-      console.log("UPDATED ACTIVE ITEM STATUS", response);
-      router.refresh();
-    },
-  });
-
-  // removes the item from the database
-  const handleDeleteItem = (id: string) => {
-    deleteMutate({ id: id });
-  };
-
-  // update the item to active or not active
-  // the item will have a strike through if it is not active
-  const handleUpdateActive = (id: string) => {
-    console.log("MARK ITEM AS COMPLETE: ", id);
-    updateActiveMutate({ id: id, active: !shoppingItem.active });
-  }
-
+  
   // TODO: Style component to look like an item in a list
   return (
     <Card>
@@ -52,14 +27,14 @@ export default function ItemsList({
       <Button
         type="button"
         className="h-8 w-8"
-        onClick={() => handleUpdateActive(shoppingItem.id)}
+        onClick={() => updateActive(shoppingItem.id, shoppingItem.active)}
       >
         <CheckCircleIcon />
       </Button>
       <Button
         type="button"
         className="h-8 w-8"
-        onClick={() => handleDeleteItem(shoppingItem.id)}
+        onClick={() => deleteItem(shoppingItem.id)}
       >
         <XCircleIcon />
       </Button>
