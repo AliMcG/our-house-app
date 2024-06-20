0ed882c32ed5e9769f8f9bdb83d6444c78329f5b
webshuriken <webshuriken@gmail.com>
2024-06-20 19:45:48 +0100

    Implement delete procedure to remove a list of chores by its id from the database

--- src/server/api/routers/chores.ts
+++ src/server/api/routers/chores.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
@@ -9,6 +10,25 @@ export const choresRouter = createTRPCRouter({
       return ctx.db.chores.findMany({
         where: {
           createdBy: { id: ctx.session.user.id }
+        }
+      });
+    }),
+  create: protectedProcedure
+    .input(z.object({ title: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.chores.create({
+        data: {
+          title: input.title,
+          createdBy: { connect: { id: ctx.session.user.id } },
+        },
+      });
+    }),
+  delete: protectedProcedure
+    .input(z.object({ id: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.chores.delete({
+        where: {
+          id: input.id
         }
       });
     }),
