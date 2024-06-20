e5855f1dff8ecbd77ed787af497de15f5a624689
webshuriken <webshuriken@gmail.com>
2024-06-20 14:27:50 +0100

    Improve the feedback when item is marked as complete with increased clickable area

--- src/app/(household)/shoppingLists/[slug]/ItemsListCard.tsx
+++ src/app/(household)/shoppingLists/[slug]/ItemsListCard.tsx
@@ -24,17 +24,19 @@ export default function ItemsListCard({
   // styles the item when is marked as completed
   const cardStyles = !active ? "bg-red" : "bg-white";
   const textStyles = !active ? "line-through" : "none";
+  // for now the active button is not styled, defaulting to theme colour
+  const buttonStyles = !active ? "bg-gray-400" : "";
 
   return (
     <Card className={`grid grid-cols-[32px_1fr_32px] gap-2 w-full px-2 py-4 ${cardStyles}`}>
       <Button
         type="button"
-        className="h-8 w-8"
+        className={`h-8 w-8 ${buttonStyles}`}
         onClick={() => updateActive(id, active)}
         >
         <CheckCircleIcon />
       </Button>
-        <p className={textStyles}>{name} <span className="font-bold">x{quantity}</span></p>
+        <p className={textStyles} onClick={() => updateActive(id, active)}>{name} <span className="font-bold">x{quantity}</span></p>
       <Button
         type="button"
         className="h-8 w-8"
