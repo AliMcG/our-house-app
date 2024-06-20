8db1dc128bc22c8b421e815756bf9a1d730cf170
webshuriken <webshuriken@gmail.com>
2024-06-20 19:45:19 +0100

    Implemented delete functionality in ChoreCard to delete a list of chores

--- src/app/(household)/chores/ChoresCard.tsx
+++ src/app/(household)/chores/ChoresCard.tsx
@@ -1,23 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Card from "@/app/_components/Card";
 import Button from "@/app/_components/Button";
 import { XCircleIcon } from "@heroicons/react/24/solid";
 import type { Chores } from "@prisma/client";
 import Link from "next/link";
+import ConfirmDeleteModal from "@/app/_components/modals/ConfirmDeleteModal";
+import { useRouter } from "next/navigation";
+import { api } from "@/trpc/react";
 
-/** As the api mutation in this Component is interacting with a Page (Server Component)
- * we need to use `router.refresh()` to invaliadate the cached data in the Page (Server Component).
- * If the cache data is in a "use client" component then we can use `api.useUtils()` hook instead.
- * further reading here: https://trpc.io/docs/client/react/useUtils
- */
 
 export default function ChoresCard({
   choresList,
 }: {
   choresList: Chores;
 }) {
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+
+  const router = useRouter();
+  const { mutate } = api.chores.delete.useMutation({
+    onSuccess: (response) => {
+      console.log("DELETED chores list", response);
+      setIsConfirmModalOpen(false)
+      router.refresh();
+    },
+  });
+
+  // uses a modal to confirm Selection before heading to the server
+  const deleteList = () => {
+    mutate({ id: choresList.id });
+  };
+  const confirmDeleteById = () => {
+    setIsConfirmModalOpen(true);
+  };
 
   return (
     <>
@@ -28,10 +44,18 @@ export default function ChoresCard({
         <Button
           type="button"
           className="h-8 w-8"
+          onClick={() => confirmDeleteById()}
         >
           <XCircleIcon />
         </Button>
       </Card>
+      <ConfirmDeleteModal
+        confirmDelete={deleteList}
+        isConfirmModalOpen={isConfirmModalOpen}
+        setIsConfirmModalOpen={setIsConfirmModalOpen}
+      >
+        <p>You are about to delete the chores: {choresList.title}</p>
+      </ConfirmDeleteModal>
     </>
   );
 }
