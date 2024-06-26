264019589af716f0a727dca53089e8e259be583a
webshuriken <webshuriken@gmail.com>
2024-06-26 14:38:45 +0100

    refactor: Update ChoresListItemResponseType in types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,3 +11,18 @@ export type ListItemResponseType = {
   listID?: string;
   status: 'list items found' | 'list not found' | 'strange, list id not found' | 'list items not found';
 }
+
+// TODO: we can get the type from the prisma schema, do we need this?
+export type ChoresItemType = { 
+  id: string;
+  name: string;
+  completedBy: string | null;
+  active: boolean;
+  choresId: string | null;
+}
+
+export type ChoresListItemResponseType = {
+  items?: ChoresItemType[];
+  listID?: string;
+  status: 'list items found' | 'list not found' | 'strange, list id not found' | 'list items not found';
+}
