81d59e6d038e822fb290898b13f3bb783861969f
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-06-27 21:11:50 +0100

    Add method to edit on click of name - example idea

--- src/app/(household)/shoppingLists/ShoppingListCard.tsx
+++ src/app/(household)/shoppingLists/ShoppingListCard.tsx
@@ -22,6 +22,8 @@ export default function ShoppingListCard({
   shoppingList: ShoppingList;
 }) {
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [newName, setNewName] = useState("");
 
   const router = useRouter();
   const { mutate } = api.shoppingList.delete.useMutation({
@@ -31,18 +33,49 @@ export default function ShoppingListCard({
       router.refresh();
     },
   });
+  const { mutate: editMutate } = api.shoppingList.edit.useMutation({
+    onSuccess: (response) => {
+      console.log("Edited shoping list", response);
+      router.refresh();
+    },
+  });
   const deleteList = () => {
     mutate({ id: shoppingList.id });
   };
   const confirmDeleteById = () => {
     setIsConfirmModalOpen(true);
   };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNameBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsEditing(false);
+    editMutate({ id: shoppingList.id, title: newName})
+  };
+
+  const handleDoubleClick = () => {
+    setIsEditing(true)
+  }
   return (
     <>
+    {/* <Link href={`/shoppingLists/${shoppingList.title}`}> */}
       <Card>
-        <Link href={`/shoppingLists/${shoppingList.title}`}>
-          {shoppingList.title}
-        </Link>
+      {isEditing ? (
+          <input
+            type="text"
+            value={newName}
+            onChange={handleNameChange}
+            onBlur={handleNameBlur}
+            autoFocus
+          />
+        ) : (
+        <div onClick={handleDoubleClick}>
+        {shoppingList.title}
+        </div>
+        )}
+       
         <Button
           type="button"
           className="h-8 w-8"
@@ -51,6 +84,7 @@ export default function ShoppingListCard({
           <XCircleIcon />
         </Button>
       </Card>
+      {/* </Link> */}
       <ConfirmDeleteModal
         confirmDelete={deleteList}
         isConfirmModalOpen={isConfirmModalOpen}
