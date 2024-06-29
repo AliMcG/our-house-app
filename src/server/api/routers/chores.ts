b965f47ad3ba419328fe245f34ec9e896ecc72d1
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-06-29 12:07:21 +0100

    Add edit route to chores router

--- src/server/api/routers/chores.ts
+++ src/server/api/routers/chores.ts
@@ -1,18 +1,14 @@
 import { z } from "zod";
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 
 export const choresRouter = createTRPCRouter({
-  list: protectedProcedure
-    .query(({ ctx }) => {
-      return ctx.db.chores.findMany({
-        where: {
-          createdBy: { id: ctx.session.user.id }
-        }
-      });
-    }),
+  list: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.chores.findMany({
+      where: {
+        createdBy: { id: ctx.session.user.id },
+      },
+    });
+  }),
   create: protectedProcedure
     .input(z.object({ title: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
@@ -23,13 +19,25 @@ export const choresRouter = createTRPCRouter({
         },
       });
     }),
+  edit: protectedProcedure
+    .input(z.object({ id: z.string().min(1), title: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.chores.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          title: input.title,
+        },
+      });
+    }),
   delete: protectedProcedure
     .input(z.object({ id: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
       return ctx.db.chores.delete({
         where: {
-          id: input.id
-        }
+          id: input.id,
+        },
       });
     }),
 });
