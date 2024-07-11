167dcdaaf8b36de880e3dc97abbd192f6f48924b
webshuriken <webshuriken@gmail.com>
2024-07-11 15:32:30 +0100

    chore: Update shopping list test to ensure successful creation of a new list

--- cypress/e2e/shopping-lists.cy.ts
+++ cypress/e2e/shopping-lists.cy.ts
@@ -2,7 +2,7 @@
  * Shopping list page test suite
  * 
  * Test LIST steps:
- * 1. Visit shopping list page
+ * 1. Make sure we can visit shopping list page (without it we can't test the rest)
  * 2. Create new list: FUNCTIONAL TEST LIST
  * 3. Edit list name: FUNCTIONAL TEST LIST -> FUNCTIONAL TEST LIST EDITED
  * 3. Delete list
@@ -14,6 +14,27 @@
  * 4. Delete item
  */
 
+describe('Test suite for shoppinglists-route', () => {
+  before(() => {
+    // login to the app
+    cy.login();
+    cy.visit('http://localhost:3000/shoppingLists');
+  });
+
+  after(() => {
+    // make sure you logout from the app
+    cy.logout();
+  });
+
+  it('creates new list', () => {
+    const listName = 'FUNCTIONAL TEST LIST';
+    cy.get('[data-cy="shoppinglist-create-input"]').type(`${listName} {enter}`);
+    cy.get('[data-cy="ShoppingListCard"]').contains(listName);
+    cy.wait(500)
+  });
+});
+
+/*
 describe('Test suite for shoppinglists-route', () => {
   before(() => {
     cy.visit('http://localhost:3000/')
@@ -33,6 +54,5 @@ describe('Test suite for shoppinglists-route', () => {
     cy.get('[data-cy="ShoppingListCard"]').contains("FUNCTIONAL TEST LIST").get('[data-cy="ShoppingListCard-delete-FUNCTIONAL TEST LIST"]').click()
     cy.get('[data-cy="confirmModal-Delete"]').click()
   })
-
-
 })
+*/
