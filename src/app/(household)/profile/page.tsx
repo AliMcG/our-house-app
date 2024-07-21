0c3082daf8a99a505676185be415175abbc6d429
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-07-21 11:01:38 +0100

    Add create new household component to profile page

--- src/app/(household)/profile/page.tsx
+++ src/app/(household)/profile/page.tsx
@@ -1,12 +1,11 @@
-import { api } from "@/trpc/server";
+import AddHouseHoldForm from "./AddHouseholdForm";
 
-export default async function ShoppingLists() {
+export default async function Profile() {
   /** By default pages are server components in next.js 14
    * This means we can use "@/trpc/server" to query our database.
    * Which should result in faster load times for the data.
    */
-  const shoppingList = await api.shoppingList.list.query();
- 
+
   return (
     <article className="flex min-h-screen flex-col bg-white pt-16 text-slate-800">
       <header className="flex items-center justify-center p-4">
@@ -15,8 +14,11 @@ export default async function ShoppingLists() {
         </h1>
       </header>
       <div className="mt-5 flex justify-center">
-        card user 
-        card for household 
+        <div className="flex flex-col gap-4">
+          <p>card user </p>
+          <p>card for household </p>
+          <AddHouseHoldForm />
+        </div>
       </div>
     </article>
   );
