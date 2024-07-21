30af388fc0a3b2add4301ca904aaf806535df780
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-07-21 12:41:56 +0100

    Add list of households to profile and Add basic edit household component

--- src/app/(household)/profile/page.tsx
+++ src/app/(household)/profile/page.tsx
@@ -1,16 +1,26 @@
 import AddHouseHoldForm from "./AddHouseholdForm";
+import { api } from "@/trpc/server";
+import EditHouseHoldForm from "./EditHouseholdForm";
 
 export default async function Profile() {
   /** By default pages are server components in next.js 14
    * This means we can use "@/trpc/server" to query our database.
    * Which should result in faster load times for the data.
    */
+  const householdList = await api.householdRouter.list.query();
 
   return (
     <article className="flex min-h-screen flex-col bg-white pt-16 text-slate-800">
       <header className="flex items-center justify-center p-4">
         <h1 className="text-center text-3xl font-extrabold tracking-tight md:text-[2rem] lg:text-[3rem] xl:text-[4rem]">
           Profile
+          {householdList.map((household, index) => {
+            return (
+              <p key={index + household.id} className="p-4 text-base" >
+                {household.name} with {household.id}
+              </p>
+            );
+          })}
         </h1>
       </header>
       <div className="mt-5 flex justify-center">
@@ -18,6 +28,7 @@ export default async function Profile() {
           <p>card user </p>
           <p>card for household </p>
           <AddHouseHoldForm />
+          <EditHouseHoldForm />
         </div>
       </div>
     </article>
