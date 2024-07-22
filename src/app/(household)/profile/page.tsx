4f0d40ab12fb2eed0aa23cc8ecfed38bace2f45f
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-07-22 20:46:14 +0100

    Tidy up UI for Profile for deveolpment

--- src/app/(household)/profile/page.tsx
+++ src/app/(household)/profile/page.tsx
@@ -1,6 +1,6 @@
 import AddHouseHoldForm from "./AddHouseholdForm";
 import { api } from "@/trpc/server";
-import EditHouseHoldForm from "./EditHouseholdForm";
+import AddUserToHouseHoldForm from "./EditHouseholdForm";
 
 export default async function Profile() {
   /** By default pages are server components in next.js 14
@@ -12,23 +12,19 @@ export default async function Profile() {
   return (
     <article className="flex min-h-screen flex-col bg-white pt-16 text-slate-800">
       <header className="flex items-center justify-center p-4">
-        <h1 className="text-center text-3xl font-extrabold tracking-tight md:text-[2rem] lg:text-[3rem] xl:text-[4rem]">
-          Profile
-          {householdList.map((household, index) => {
-            return (
-              <p key={index + household.id} className="p-4 text-base" >
-                {household.name} with {household.id}
-              </p>
-            );
-          })}
-        </h1>
+        <h1 className="flex text-3xl font-extrabold">Your households:</h1>
+        {householdList.map((household, index) => {
+          return (
+            <p key={index + household.id} className="p-4 text-base font-bold">
+              {household.name} with {household.id}
+            </p>
+          );
+        })}
       </header>
       <div className="mt-5 flex justify-center">
-        <div className="flex flex-col gap-4">
-          <p>card user </p>
-          <p>card for household </p>
+        <div className="flex gap-4">
           <AddHouseHoldForm />
-          <EditHouseHoldForm />
+          <AddUserToHouseHoldForm />
         </div>
       </div>
     </article>
