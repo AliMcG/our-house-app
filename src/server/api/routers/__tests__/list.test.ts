600ef7b9ba998bb087a5f5775332a028f50077af
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-07-29 21:28:23 +0100

    Update working unit test for shoppingList.list route

--- src/server/api/routers/__tests__/list.test.ts
+++ src/server/api/routers/__tests__/list.test.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@jest/globals"
-import { createContextInner, createTRPCContext } from '@/server/api/trpc';
+import { env } from "@/env";
+import { createContextInner } from '@/server/api/trpc';
 import { createCaller } from '@/server/api/root';
 import type { Session } from "next-auth";
 
@@ -9,7 +10,7 @@ import type { Session } from "next-auth";
 jest.mock("superjson", () => ({
   superjson: jest.fn()
 }))
-jest.mock("~/env", () => ({
+jest.mock("@/env", () => ({
   env: jest.fn(),
  }))
  jest.mock("next-auth", () => ({
@@ -19,23 +20,20 @@ jest.mock("~/env", () => ({
 test('List all shoppingLists', async () => {
   const mockSession: Session = {
     expires: new Date().toISOString(),
-    user: { id: "test-user" },
+    user: { id: env.UNIT_TESTER_ID },
   };
- 
-  /**
-   * createTRPCContext requires new Headers()
-   */
-  // const caller = createCaller(await createTRPCContext({ headers: new Headers() }));
-  const caller = createCaller(await createContextInner());
 
-  /**
-   * The route for "hello" is a public route
-   */
-  const shoppingLists = await caller.post.hello({ text: "hi there"})
-    /**
-   * The route for "getSecretMessage" is a protected route
-   */
-  // const shoppingLists = await caller.post.getSecretMessage()
-  console.log("shoppingLists", shoppingLists)
+  const caller = createCaller(await createContextInner({session: mockSession}));
+
+  const shoppingLists = await caller.shoppingList.list()
+
+  expect(Array.isArray(shoppingLists)).toBe(true);
+  shoppingLists.forEach(list => {
+    expect(typeof list).toBe('object');
+    expect(list).toHaveProperty('id');
+    expect(list).toHaveProperty('title');
+    expect(list).toHaveProperty('createdAt');
+    expect(list).toHaveProperty('updatedAt');
+  });
 
 });
