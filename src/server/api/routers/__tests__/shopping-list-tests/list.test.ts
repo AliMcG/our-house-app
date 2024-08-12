e0886fce9bf13a6dfed9677467bf725928394b9a
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-08-12 21:39:27 +0100

    Update shoppingList list test to test for tRPC errors

--- src/server/api/routers/__tests__/shopping-list-tests/list.test.ts
+++ src/server/api/routers/__tests__/shopping-list-tests/list.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@jest/globals"
+import { test, expect, describe, it } from "@jest/globals"
 import { createContextInner } from '@/server/api/trpc';
 import { createCaller } from '@/server/api/root';
 import type { Session } from "next-auth";
@@ -18,23 +18,39 @@ jest.mock("@/env", () => ({
   getServerSession: jest.fn(),
  }))
 
-test('List all shoppingLists', async () => {
+
+describe("List", () => {
   const mockSession: Session = {
     expires: new Date().toISOString(),
     user: { id: process.env.UNIT_TESTER_ID as string },
   };
-
-  const caller = createCaller(await createContextInner({session: mockSession}));
-
-  const shoppingLists = await caller.shoppingList.list()
-
-  expect(Array.isArray(shoppingLists)).toBe(true);
-  shoppingLists.forEach(list => {
-    expect(typeof list).toBe('object');
-    expect(list).toHaveProperty('id');
-    expect(list).toHaveProperty('title');
-    expect(list).toHaveProperty('createdAt');
-    expect(list).toHaveProperty('updatedAt');
+  const mockErrorSession: Session = {
+    expires: new Date().toISOString(),
+    user: { id: "No ID" },
+  };
+  
+  it('List all shoppingLists', async () => {
+  
+    const caller = createCaller(await createContextInner({session: mockSession}));
+    
+    const shoppingLists = await caller.shoppingList.list()
+  
+    expect(Array.isArray(shoppingLists)).toBe(true);
+    shoppingLists.forEach(list => {
+      expect(typeof list).toBe('object');
+      expect(list).toHaveProperty('id');
+      expect(list).toHaveProperty('title');
+      expect(list).toHaveProperty('createdAt');
+      expect(list).toHaveProperty('updatedAt');
+    });
   });
 
-});
+  it("should throw an error if no user ID", async () => {
+ 
+    const caller = createCaller(await createContextInner({session: mockErrorSession}));
+    
+    await expect(caller.shoppingList.list()).rejects.toThrow("ser not found")
+
+  })
+})
+
