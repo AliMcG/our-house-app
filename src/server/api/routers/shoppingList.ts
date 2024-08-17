6eda0bea61214263304fdfeee1f9983b519d0371
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-08-17 16:56:32 +0100

    Update logic for edit and delete routes - to be more readable.

--- src/server/api/routers/shoppingList.ts
+++ src/server/api/routers/shoppingList.ts
@@ -110,7 +110,11 @@ export const shoppingListRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ title: z.string().min(1, { message: "Title can not be blank"}) }))
+    .input(
+      z.object({
+        title: z.string().min(1, { message: "Title can not be blank" }),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       if (ObjectId.isValid(ctx.session?.user?.id)) {
         try {
@@ -135,60 +139,67 @@ export const shoppingListRouter = createTRPCRouter({
     }),
 
   edit: protectedProcedure
-    .input(z.object({ id: z.string().min(1), title: z.string().min(1, { message: "Title can not be blank"}) }))
+    .input(
+      z.object({
+        id: z.string().min(1, { message: "Select a list to edit" }),
+        title: z.string().min(1, { message: "Title can not be blank" }),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
-      try {
-        const shoppingList = await ctx.db.shoppingList.findUnique({
-          where: { id: input.id },
-        });
-
-        if (!shoppingList) {
+      const shoppingList = await ctx.db.shoppingList.findUnique({
+        where: { id: input.id },
+      });
+      if (shoppingList) {
+        try {
+          return await ctx.db.shoppingList.update({
+            where: {
+              id: input.id,
+            },
+            data: {
+              title: input.title,
+            },
+          });
+        } catch (error) {
           throw new TRPCError({
-            code: "NOT_FOUND",
-            message: "Shopping list not found",
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Failed to update shopping list",
           });
         }
-
-        return await ctx.db.shoppingList.update({
-          where: {
-            id: input.id,
-          },
-          data: {
-            title: input.title,
-          },
-        });
-      } catch (error) {
+      } else {
         throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to update shopping list",
+          code: "NOT_FOUND",
+          message: "Shopping list not found",
         });
       }
     }),
 
   delete: protectedProcedure
-    .input(z.object({ id: z.string().min(1) }))
+    .input(
+      z.object({
+        id: z.string().min(1, { message: "Select a list to delete" }),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
-      try {
-        const shoppingList = await ctx.db.shoppingList.findUnique({
-          where: { id: input.id },
-        });
-
-        if (!shoppingList) {
+      const shoppingList = await ctx.db.shoppingList.findUnique({
+        where: { id: input.id },
+      });
+      if (shoppingList) {
+        try {
+          return await ctx.db.shoppingList.delete({
+            where: {
+              id: input.id,
+            },
+          });
+        } catch (error) {
           throw new TRPCError({
-            code: "NOT_FOUND",
-            message: "Shopping list not found",
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Failed to delete shopping list",
           });
         }
-
-        return await ctx.db.shoppingList.delete({
-          where: {
-            id: input.id,
-          },
-        });
-      } catch (error) {
+      } else {
         throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to delete shopping list",
+          code: "NOT_FOUND",
+          message: "Shopping list not found",
         });
       }
     }),
