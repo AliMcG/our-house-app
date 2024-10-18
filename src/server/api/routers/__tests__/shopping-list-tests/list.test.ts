2b2a0693a5d89512819111b8cf2d2356948e8926
ali <mcgill.alistair@yahoo.co.uk>
2024-10-18 22:05:06 +0100

    Update unit tests for shopping list List

--- src/server/api/routers/__tests__/shopping-list-tests/list.test.ts
+++ src/server/api/routers/__tests__/shopping-list-tests/list.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it } from "@jest/globals";
+import { expect, describe } from "@jest/globals";
 import { createContextInner } from "@/server/api/trpc";
 import { createCaller } from "@/server/api/root";
 import {
@@ -7,6 +7,7 @@ import {
   mockSession,
 } from "../../utils/testHelpers";
 import dotenv from "dotenv";
+import { TRPCError } from "@trpc/server";
 dotenv.config();
 /**
  * These modules required mocking for Jest to work.
@@ -20,40 +21,77 @@ jest.mock("@/env", () => ({
 jest.mock("next-auth", () => ({
   getServerSession: jest.fn(),
 }));
+let caller: ReturnType<typeof createCaller>;
+let inValidCaller: ReturnType<typeof createCaller>;
+let newListId: string;
+const editDate = new Date();
+const input = {
+  title: `UNIT TEST SHOPPING LIST: List - ${editDate.toLocaleDateString()}`,
+};
 
-describe("the list route", () => {
-  it("lists all shoppingLists", async () => {
-    const caller = createCaller(
-      await createContextInner({ session: mockSession }),
-    );
-    const shoppingLists = await caller.shoppingList.list();
-
-    expect(Array.isArray(shoppingLists)).toBe(true);
-    shoppingLists.forEach((list) => {
-      expect(typeof list).toBe("object");
-      expect(list).toHaveProperty("id");
-      expect(list).toHaveProperty("title");
-      expect(list).toHaveProperty("createdAt");
-      expect(list).toHaveProperty("updatedAt");
-    });
-  });
-
-  it("should throw an error if no user ID", async () => {
-    const caller = createCaller(
-      await createContextInner({ session: mockErrorSessionNoID }),
-    );
-    await expect(caller.shoppingList.list()).rejects.toThrow(
-      "User is undefined",
-    );
-  });
-
-  it("should return an empty array if no households for this user.", async () => {
-    const caller = createCaller(
-      await createContextInner({ session: mockErrorSessionUnknownID }),
-    );
-
-    const shoppingLists = await caller.shoppingList.list();
-    expect(Array.isArray(shoppingLists)).toBe(true);
-    expect(shoppingLists).toHaveLength(0);
-  });
+beforeAll(async () => {
+  caller = createCaller(await createContextInner({ session: mockSession }));
+  inValidCaller = createCaller(await createContextInner({ session: mockErrorSessionNoID }))
+  const shoppingListToDelete = await caller.shoppingList.create(input);
+  newListId = shoppingListToDelete.id
 });
+afterAll(async () => {
+  await caller.shoppingList.delete({ id: newListId })
+});
+
+describe('Feature: List shopping lists', () => {
+  describe('Scenario: invalid user', () => {
+    describe('Given an invalid user is trying to list all shopping lists', () => {
+      test('Then a error should be thrown', async () => {
+        await expect(inValidCaller.shoppingList.list()).rejects.toThrow(
+          "User is undefined",
+        );
+      })
+      test('And the error should be a TRPCError', async () => {
+        expect.assertions(2)
+        try {
+          await inValidCaller.shoppingList.list()
+        } catch (error) {
+          expect(error).toBeInstanceOf(TRPCError)
+          if (error instanceof TRPCError) {
+            expect(error.code).toStrictEqual("UNAUTHORIZED")
+          }
+        }
+      })
+    })
+  })
+  describe('Scenario: user has no shopping list', () => {
+    describe('Given a valid user has no shopping lists', () => {
+      describe('When the List method is called', () => {
+        test("It should return an empty array", async () => {
+          const caller = createCaller(
+            await createContextInner({ session: mockErrorSessionUnknownID }),
+          );
+          const shoppingLists = await caller.shoppingList.list();
+          expect(Array.isArray(shoppingLists)).toBe(true);
+          expect(shoppingLists).toHaveLength(0);
+        });
+      })
+    })
+  })
+  describe("Scenario: Successfully edits shopping list", () => {
+    describe('Given that the user is valid and has access to some lists', () => {
+      describe('When the List method is called', () => {
+        test("It lists all shoppingLists", async () => {
+
+          const shoppingLists = await caller.shoppingList.list();
+      
+          expect(Array.isArray(shoppingLists)).toBe(true);
+          expect(shoppingLists.length).toBeGreaterThanOrEqual(1);
+          shoppingLists.forEach((list) => {
+            expect(typeof list).toBe("object");
+            expect(list).toHaveProperty("id");
+            expect(list).toHaveProperty("title");
+            expect(list).toHaveProperty("createdAt");
+            expect(list).toHaveProperty("updatedAt");
+          });
+        });
+      })
+    })
+  });
+})
