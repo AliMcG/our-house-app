be8ae8acbcac7464071c1b31af6c6fb656339936
AliMcGmc <mcgill.alistair@yahoo.co.uk>
2025-11-09 11:10:18 +0000

    add eslint override rules for test files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,13 +17,6 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/non-nullable-type-assertion-style": "off",
-    // "@typescript-eslint/consistent-type-imports": [
-    //   "warn",
-    //   {
-    //     prefer: "type-imports",
-    //     fixStyle: "inline-type-imports",
-    //   },
-    // ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
@@ -33,6 +26,27 @@ const config = {
       },
     ],
   },
+  overrides: [
+    {
+      // Target all files ending with .test.ts or .spec.ts
+      files: ["**/*.test.ts", "**/*.spec.ts", "**/__tests__/**/*"],
+      // Enable Jest-specific environments
+      env: {
+        jest: true,
+      },
+      // Extend configurations for testing if you have them
+      // extends: ["plugin:jest/recommended"],
+      rules: {
+        // You can selectively disable or modify rules for test files here.
+        // For example, to disable the unbound-method rule:
+        "@typescript-eslint/unbound-method": "off",
+
+        // "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+        // "@typescript-eslint/no-misused-promises": "off", 
+        // "@typescript-eslint/explicit-function-return-type": "off", 
+      },
+    },
+  ],
 };
 
 module.exports = config;
