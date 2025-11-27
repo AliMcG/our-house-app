4553967e40b27bb1fab29aacf03218fea189afc9
webshuriken <webshuriken@gmail.com>
2025-11-27 19:01:59 +0000

    feat: rearrange skeleton component to use compositional approach
    as skeletons are UI placeholders not semantic UI elements
    this approach will make it straight forward to build custom skeletons

--- src/app/_components/ui/skeleton.tsx
+++ src/app/_components/ui/skeleton.tsx
@@ -1,13 +1,47 @@
 import { cn } from "@/app/utils/cn";
+import { props } from "node_modules/cypress/types/bluebird";
+import React from 'react';
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+
+/*
+  - Uses a compositional approach as skeletons are UI placeholders not Semantic UI elements
+  - as visual placeholders a `forwardRef` is not required
+  - no content or layout logic
+  - focused on shape + animation
+*/
+export function Skeleton({ className, children, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton-group"
+      className={cn("flex flex-col gap-2 border animate-pulse", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
+
+// shared skeleton styles
+const base = "bg-accent rounded-md"
+
+/* Componsitional Variants */
+
+// Text: defaults to full width of container with round edges
+Skeleton.Text = function SkeletonText({
+  className,
+  ...props,
+}: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-accent animate-pulse rounded-md", className)}
+      className={cn(base, "h-4 w-full", className)}
       {...props}
     />
   )
 }
 
-export { Skeleton }
+// Image: defaults to 80px by 80px square with round edges
+
+// Button: defaults to 60px by 120px rectangle with round edges
+
+// Circle: defaults to a 80px by 80px
