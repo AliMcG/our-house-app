ca9814e6aa8a0e0515d80ac61530eceb4a6c6630
webshuriken <webshuriken@gmail.com>
2025-11-28 12:37:52 +0000

    feat: update chores loading with custom shadcn compositional Skeleton component

--- src/app/(household)/chores/loading.tsx
+++ src/app/(household)/chores/loading.tsx
@@ -1,32 +1,35 @@
+import { Skeleton } from "@/app/_components/ui/skeleton"
+
+
 /* Custom skeleton for the Shopping list (landing page) loader */
 export default function loading() {
   return (
-    <div className="flex flex-col animate-pulse size-full bg-white pt-[96px] p-8 gap-4">
-      <div className="flex h-6 sm:h-10 md:h-12 w-full sm:w-[280px] self-center rounded bg-gray-200"></div>
+    <div className="flex flex-col size-full bg-white pt-[96px] p-8 gap-4">
+      <Skeleton.Text className="h-6 sm:h-10 md:h-12 sm:w-[280px] self-center" />
 
       {/* Creation card */}
-      <div className="flex flex-col w-full sm:w-[280px] h-[120px] md:h-[160px] place-content-center self-center rounded-md border border-purple-300 px-4 gap-4">
-        <div className="h-2 md:h-4 rounded bg-gray-200"></div>
-        <div className="h-6 md:h-8 rounded bg-gray-200"></div>
-        <div className="h-6 md:h-8 rounded bg-gray-200"></div>
-      </div>
+      <Skeleton className="sm:w-[280px] h-[120px] md:h-[160px] self-center">
+        <Skeleton.Text className="md:h-4" />
+        <Skeleton.Text className="h-6 md:h-8" />
+        <Skeleton.Button className="self-end" />
+      </Skeleton>
 
       {/* Possible shopping list card */}
       <div className="flex flex-col md:flex-row items-center sm:mt-4 md:mt-8 gap-6">
-        <div className="flex flex-col w-full max-w-[400px] h-[120px] place-content-center rounded-md border border-purple-300 px-4 gap-4">
-          <div className="h-6 md:h-8 rounded bg-gray-200"></div>
+        <Skeleton className="max-w-[400px] h-[120px]">
+          <Skeleton.Text className="h-6 md:h-8" />
           <div className="flex justify-end gap-6">
-            <div className="h-4 md:h-6 w-10 rounded bg-gray-200"></div>
-            <div className="h-4 md:h-6 w-10 rounded bg-gray-200"></div>
+            <Skeleton.Button className="h-4 md:h-6 w-10" />
+            <Skeleton.Button className="h-4 md:h-6 w-10" />
           </div>
-        </div>
-        <div className="flex flex-col w-full max-w-[400px] h-[120px] place-content-center rounded-md border border-purple-300 px-4 gap-4">
-          <div className="h-6 md:h-8 rounded bg-gray-200"></div>
+        </Skeleton>
+        <Skeleton className="max-w-[400px] h-[120px]">
+          <Skeleton.Text className="h-6 md:h-8" />
           <div className="flex justify-end gap-6">
-            <div className="h-4 md:h-6 w-10 rounded bg-gray-200"></div>
-            <div className="h-4 md:h-6 w-10 rounded bg-gray-200"></div>
+            <Skeleton.Button className="h-4 md:h-6 w-10" />
+            <Skeleton.Button className="h-4 md:h-6 w-10" />
           </div>
-        </div>
+        </Skeleton>
       </div>
 
     </div>
