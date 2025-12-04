57f60ef1362fdf0d38b4577a117c456d21db559a
webshuriken <webshuriken@gmail.com>
2025-12-04 19:13:06 +0000

    feat: update logic for invite/slug/page
    we use the new values returned by findInviteByToken method
    simplifying the display logic. Both registered and new
    users will be asked to login before accepting invite

--- src/app/invite/[token]/page.tsx
+++ src/app/invite/[token]/page.tsx
@@ -5,37 +5,26 @@ import AuthButton from './InviteAuthButton';
  * Unauthenticated route to handle household invite acceptance
  */
 export default async function InvitePage({ params }: { params: { token: string } }) {
+  // For this stage we don't need to know if user is registered.
+  // We only need to make sure the invite token is valid
+  const householdInvite = await api.householdUserRouter.findInviteByToken.query(
+      { inviteToken: params.token }
+  );
 
-    const householdInvite = await api.householdUserRouter.findInviteByToken.query(
-        { inviteToken: params.token }
-    );
-
-    if (!householdInvite) {
-        return <div>Error: Invite details not available.</div>;
-    }
-
-    const { household, inviterUser, invitedEmail, invitedUserId  } = householdInvite;
-
-    // if user is already registered then on clicking the "accept" button
-    // update db table householdUser and redirect to household page
-    // else redirect to google oauth flow with callback to api/auth/google-invite-callback
-    if (invitedUserId) {
-        return (
-            <div>   
-                <p>You've been invited to join the household <strong>{household.name}</strong> by {inviterUser.name}.</p>
-                <p>The invite is for: <strong>{invitedEmail}</strong>.</p>
-                <p>Please sign in to accept the invite.</p>
-                <AuthButton size="small" token={params.token} />
-            </div>
-        );
-    }
-
-    return (
-        <div>
-            <p>You've been invited to join the household <strong>{household.name}</strong> by {inviterUser.name}.</p>
-            <p>The invite is for: <strong>{invitedEmail}</strong>.</p>
+  return (
+    <div>
+      {
+        !householdInvite ? (
+          <div>Error: Invite details not available.</div>
+        ) : (
+          <div>
+            <p>You've been invited to join the household <strong>{householdInvite.household.name}</strong> by {householdInvite.inviterUser.name}.</p>
+            <p>The invite is for: <strong>{householdInvite.invitedEmail}</strong>.</p>
             <p>Please sign in with Google to accept the invite.</p>
             <AuthButton size="small" token={params.token} />
-        </div>
-    );
+          </div>
+        )
+      }
+    </div>
+  );
 }
