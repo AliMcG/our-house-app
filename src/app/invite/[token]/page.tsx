d486415a57054b586e0415aca8cffe1d3cb6c63e
webshuriken <webshuriken@gmail.com>
2025-12-05 22:23:20 +0000

    feat: logic for the landing page for household invite link
    This is the page the user lands on when they click
    on the email invite link

--- src/app/invite/[token]/page.tsx
+++ src/app/invite/[token]/page.tsx
@@ -1,30 +1,38 @@
 import { api } from "@/trpc/server";
+import { redirect } from "next/navigation";
 import AuthButton from './InviteAuthButton';
 
 /**
  * Unauthenticated route to handle household invite acceptance
+ * used 'params' to get token as it is part of the dynamic route
  */
 export default async function InvitePage({ params }: { params: { token: string } }) {
-  // For this stage we don't need to know if user is registered.
-  // We only need to make sure the invite token is valid
-  const householdInvite = await api.householdUserRouter.findInviteByToken.query(
-      { inviteToken: params.token }
-  );
+  const token = params.token;
 
+  // safety check for missing token
+  if (!token) throw {code: "missing_token"}
+
+  // validate token via tRPC
+  let invite;
+  try {
+    invite = await api.householdUserRouter.findInviteByToken.query({ 
+      inviteToken: token 
+    });
+    
+    if (!invite) throw {code: "invalid_invite"};
+
+  }catch(e) {
+    redirect(`/invite/error?code={e.code}`)
+  }
+
+  // user must signin to their Google account to accept invite
+  // passing in the url redirect after Google OAuth
   return (
     <div>
-      {
-        !householdInvite ? (
-          <div>Error: Invite details not available.</div>
-        ) : (
-          <div>
-            <p>You've been invited to join the household <strong>{householdInvite.household.name}</strong> by {householdInvite.inviterUser.name}.</p>
-            <p>The invite is for: <strong>{householdInvite.invitedEmail}</strong>.</p>
-            <p>Please sign in with Google to accept the invite.</p>
-            <AuthButton size="small" token={params.token} />
-          </div>
-        )
-      }
+      <p>You have been invited to join the household <strong>{invite.household.name}</strong> by {invite.inviterUser.name}.</p>
+      <p>The invite is for: <strong>{invite.invitedEmail}</strong>.</p>
+      <p>Please sign in with Google to accept the invite.</p>
+      <AuthButton size="small" completeInviteUrl={`/invite/complete?inviteToken=${token}`} />
     </div>
   );
 }
