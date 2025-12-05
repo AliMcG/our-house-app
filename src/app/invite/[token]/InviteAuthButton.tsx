a064676f0d34636283d078829dc99927940fe3d9
webshuriken <webshuriken@gmail.com>
2025-12-05 22:25:33 +0000

    feat: add urlCallback for Google OAuth in InviteAuthButton.tsx
    This update will redirect the user to the complete invitation
    page after they have successfully logged in with
    their Google account

--- src/app/invite/[token]/InviteAuthButton.tsx
+++ src/app/invite/[token]/InviteAuthButton.tsx
@@ -5,7 +5,7 @@ import Button from "@/app/_components/Button";
 
 export type Size = "small" | "normal" | "large"
 
-export default function AuthButton({ size, token }: { size: Size, token: string }) {
+export default function AuthButton({ size, completeInviteUrl }: { size: Size, completeInviteUrl: string }) {
     const { data: sessionData } = useSession();
 
     return (
@@ -13,7 +13,7 @@ export default function AuthButton({ size, token }: { size: Size, token: string 
             data-cy="auth-button"
             intent={sessionData ? "signout" : "signin"}
             size={`${size}`}
-            onClick={sessionData ? () => void signOut() : () => void signIn('google', { state: token })}
+            onClick={sessionData ? () => void signOut() : () => void signIn('google', { callbackUrl: completeInviteUrl })}
         >{sessionData ? "Sign out" : "Sign in"}</Button>
     );
 }
