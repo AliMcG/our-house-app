0645edb61611bb256ff738a7fc609b9210ebde33
Carlos E Alford <webshuriken@gmail.com>
2025-12-14 22:09:49 +0000

    feat: implement quick menu toggle and sidebar in Header component

--- src/app/_components/layout/header.tsx
+++ src/app/_components/layout/header.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { Button } from '../ui/button'
 import { cn } from '@/app/utils/cn'
 import {
-  Menu,
+  EllipsisVertical as Menu,
   X,
 } from 'lucide-react'
 
@@ -31,8 +31,11 @@ export default function Header({
   // resuse the description prop or use a default value
   const headerMsg = description ?? "Manage your household, lists, and tasks";
 
+  // state to manage mobile menu toggle
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <header className="grid grid-cols-[1fr_40px] grid-rows-2 w-screen h-20 pl-[60px] sm:grid-rows-[60%_40%] sm:grid-cols-[1fr_80px] md:px-4  bg-slate-50 border-b border-slate-200">
+    <header className="grid grid-cols-[1fr_40px] grid-rows-2 w-screen h-20 pl-[60px] sm:grid-rows-[60%_40%] sm:grid-cols-[1fr_80px] md:w-full md:px-4 md:grid-cols-[max(360px)_1fr] bg-slate-50 border-b border-slate-200">
       <h1 className="col-span-1 text-2xl font-bold self-end text-slate-700">
           { title }
       </h1>
@@ -41,8 +44,34 @@ export default function Header({
       </p>
       { 
         children && (
-          <section>
-            {children}
+          <section className="fixed top-0 right-0 h-screen w-[0px] z-[999] md:relative md:w-full md:h-full md:row-span-2 md:col-start-2">
+            {/* Mobile menu button */}
+            <Button
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "fixed top-[20px] right-[8px] transition-transform duration-200 ease-in-out z-50 md:hidden"
+              )}
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+            </Button>
+            {/* Sidebar */}
+            <div className={cn(
+              "fixed right-0 w-[220px] h-screen z-40 overflow-hidden transform bg-slate-50 border-l border-gray-200 transition-transform duration-200 ease-in-out md:relative md:w-full md:h-full md:border-none md:translate-x-0",
+              isOpen ? "translate-x-0" : "translate-x-[220px]"
+            )}>
+              <div className="flex flex-col h-full pt-[100px] px-2 md:py-2 md:items-end md:justify-center">
+                {children}
+              </div>
+            </div>
+            {/* Mobile overlay */}
+            {isOpen && (
+              <div
+                className="fixed inset-0 z-30 bg-gray-600 bg-opacity-75 md:hidden"
+                onClick={() => setIsOpen(false)}
+              />
+            )}
           </section>
         ) 
       }
